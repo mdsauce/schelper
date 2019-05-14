12884e9e72635f65dbf4bc0246ee0a472e7b5540
mdsauce <max.dobeck@saucelabs.com>
2019-05-14 11:48:25 -0700

    Added more known problems.

--- helper/disruptions.go
+++ helper/disruptions.go
@@ -9,4 +9,6 @@ type Disruption struct {
 }
 
 // LocalDNS represents problems with DNS servers hosted on the customer's intranet
-var LocalDNS = Disruption{Category: "Intranet DNS", GeneralSteps: "Find IP Address of local DNS servers that were used by looking for phrase 'PROXY found DNS server'.", Info: "The Local DNS server Could not be found or could not be used to resolve domain names."}
+var localDNS = Disruption{Category: "Intranet DNS", GeneralSteps: "Find IP Address of local DNS servers that were used by looking for phrase 'PROXY found DNS server'.", Info: "The Local DNS server Could not be found or could not be used to resolve domain names."}
+
+var kgpConnection = Disruption{Category: "KGP Maki Connection", GeneralSteps: "Ensure the tunnel can stay up for at least 24 hours via Looker.", Info: "This should only happen briefly, ideally never, except at the end of a tunnel's lifecycle. If it happens repeatedly there is a proble connecting the SC Client to the Maki Tunnel VM."}
--- helper/knownProblems.go
+++ helper/knownProblems.go
@@ -3,7 +3,7 @@ package helper
 // KnownProblem contains actual log entries and
 // their associated Disruption and any specific next steps
 type KnownProblem struct {
-	Name       []byte
+	Name       string
 	Disruption Disruption
 	Logs       []byte
 	NextSteps  string
@@ -12,8 +12,19 @@ type KnownProblem struct {
 // AllProblems returns all known problems
 func AllProblems() []KnownProblem {
 	var AllProblems []KnownProblem
-	var dnsResolution = KnownProblem{Name: []byte("DNS-Resolution"), Disruption: LocalDNS, Logs: []byte("MAIN DNS error: non-recoverable failure in name resolution (4) MAIN DNS error: EVUTIL_EAI_FAIL MAIN DNS error"), NextSteps: `1) Locate the DNS servers that were used from the SC logs
+	var dnsResolution = KnownProblem{Name: "DNS-Resolution", Disruption: localDNS, Logs: []byte("MAIN DNS error: non-recoverable failure in name resolution (4) MAIN DNS error: EVUTIL_EAI_FAIL MAIN DNS error"), NextSteps: `1) Locate the DNS servers that were used from the SC logs
 2) See what domain name was attempting to be resolved.  Should be a 'connecting' message prior to DNS failure`}
 	AllProblems = append(AllProblems, dnsResolution)
+
+	var earlyDisconnect = KnownProblem{Name: "Early-Disconnect", Disruption: kgpConnection, Logs: []byte("KGP libevent connection error MAIN main loop exited, return code: 5"), NextSteps: `Confirm this only happens one time, at the end of the log after the Stop signal, CTRL-C (SIGINT), is sent.  If this happens during any other time there is potential for a bad connection or a Maki that has problems maintain a connection to a client.
+	
+	This could be the customer network having problems maintaining the TCP tunnel or problems with the Keep Alive signal.  Look for any DEAD or LIVE signals in Sumo.`}
+	AllProblems = append(AllProblems, earlyDisconnect)
+
+	var noTunnelConnection = KnownProblem{Name: "No-Initial-Tunnel", Disruption: kgpConnection, Logs: []byte("sent reply 000000000000"), NextSteps: `This may not be a problem.  It can happen a handful of times before seeing the corresponding 000000000001 sent reply.  If it happens reliably and only on the customer's network then there is a problem opening a tunnel, i.e. connecting to the Maki Tunnel VM.
+	
+	Confirm that you can curl -vv a maki and the subdomain.  You should also use ping to see if there is packet loss.  Networking/IT team from customers side will probably need to intervene.`}
+	AllProblems = append(AllProblems, noTunnelConnection)
+
 	return AllProblems
 }
