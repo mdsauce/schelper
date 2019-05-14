803e58d669bb60873e16334441ad466cbcf9816e
mdsauce <max.dobeck@saucelabs.com>
2019-05-14 11:53:22 -0700

    added better variable name.

--- helper/comparison.go
+++ helper/comparison.go
@@ -6,9 +6,9 @@ import (
 
 func problem(logline []byte) (bool, KnownProblem) {
 	for _, problem := range AllProblems() {
-		body := bytes.Split(logline, []byte(" "))
+		splitline := bytes.Split(logline, []byte(" "))
 		// start at (include) 3rd element in slice
-		core := bytes.Join(body[3:], []byte(" "))
+		core := bytes.Join(splitline[3:], []byte(" "))
 		if bytes.Contains(problem.Logs, core) {
 			return true, problem
 		}
