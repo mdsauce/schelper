4302a5c71b115213eb8371e0814d9cac1954829c
mdsauce <max.dobeck@saucelabs.com>
2019-05-14 11:47:16 -0700

    Added comment to clarify.

--- helper/comparison.go
+++ helper/comparison.go
@@ -7,6 +7,7 @@ import (
 func problem(logline []byte) (bool, KnownProblem) {
 	for _, problem := range AllProblems() {
 		body := bytes.Split(logline, []byte(" "))
+		// start at (include) 3rd element in slice
 		core := bytes.Join(body[3:], []byte(" "))
 		if bytes.Contains(problem.Logs, core) {
 			return true, problem
