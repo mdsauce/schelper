c4807a2f19fcfd118803a7359fd5b33996eed700
mdsauce <max.dobeck@saucelabs.com>
2019-05-16 19:11:12 -0700

    Started adding issues found in kb217 https://wiki.saucelabs.com/display/SKB/KB217+Sauce+Connect+Troubleshooting.

--- helper/knownProblems.go
+++ helper/knownProblems.go
@@ -28,5 +28,19 @@ before seeing the corresponding 000000000001 sent reply.  If it happens reliably
 Confirm that you can curl -vv a maki and the subdomain.  You should also use ping to see if there is packet loss.  Networking/IT team from customers side will probably need to intervene.`}
 	AllProblems = append(AllProblems, noTunnelConnection)
 
+	var custSSLTLS = KnownProblem{Name: "SSL/TLS-Customer-Cert", Disruption: sslTLS, Logs: []byte("MAIN SSL verify error MAIN SSL verify error:num=19:self signed certificate in certificate chain:depth=3:/CN= KGP SSL error: certificate verify failed in SSL routines ssl3_get_server_certificate libevent connection error"), NextSteps: `We are rejecting this because we can't verify the Self Signed cert being used is real with any of the 3rd party Certificate Authorities.
+
+	Verify the client is not using some weird Custom Defined Self-Signed Certificate.  I.g. check the machine they're on with the customer, each OS will have a custom way of getting and verifying Certs are valid.  Try using the --capath <capath dir> flag.`}
+	AllProblems = append(AllProblems, custSSLTLS)
+
+	var sockMakiErr = KnownProblem{Name: "Socket-Maki-Conn", Logs: []byte("MAIN failed to connect KGP (socket error: socket error: No connection could be made because the target machine actively refused it.) MAIN failed to connect KGP socket error"), NextSteps: `Sauce labs lives at 3 IP blocks at the time of this writing.  So if we're unable to connect to a Maki or any other endpoint try pinging that endpoint from the Sauce Connect Host machine aka the host.
+	
+From this host machine ping 162.222.75.78.
+Curl 162.222.75.78:443 as the ping protocol does not use port 443 and may not be blocked. You may get Empty reply from server which is good.  You weren't blocked by a proxy or firewall and the request actually left the private network.
+Telnet as well if you want to cover all bases: telnet maki86032.miso.saucelabs.com 443.  Again you just want to make sure you don't get a 4xx Blocked by proxy blah blah blah.
+
+All of these steps are just to ensure a request can leave the private network without being blocked or filtered.`}
+	AllProblems = append(AllProblems, sockMakiErr)
+
 	return AllProblems
 }
