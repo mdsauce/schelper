d49cea96464ae8e62237fff07b51da56894312d6
mdsauce <max.dobeck@saucelabs.com>
2019-06-18 16:55:21 -0700

    Ran 'brew fetch schelper --build-from-source' to try and get true sha256.

--- schelper.rb
+++ schelper.rb
@@ -2,7 +2,7 @@ class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
   url "https://github.com/mdsauce/schelper/archive/v1.0.3.tar.gz"
-  sha256 "8ae9dfb235fa8effa896b1f27e024efb69894419de4eeb913a1dc0578f85a7f9"
+  sha256 "36d01c99ad9dea9abc5c96e3e43e91e21e2d36469ecc65d6f396b2e4d350cb49"
   depends_on "go" => :build
   version "1.0.3"
 
