d9919ef257ee0e2e5d40af4548190ba5948e84b9
mdsauce <max.dobeck@saucelabs.com>
2019-06-18 16:50:56 -0700

    Releasing v1.0.3 with new sha256.

--- schelper.rb
+++ schelper.rb
@@ -2,7 +2,7 @@ class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
   url "https://github.com/mdsauce/schelper/archive/v1.0.3.tar.gz"
-  sha256 ""
+  sha256 "0cc6f91e40699578f3183b07dc5768d0e3482996a40233e4baac3c776a2c1dbd"
   depends_on "go" => :build
   version "1.0.3"
 
