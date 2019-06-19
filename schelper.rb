8e578ec296e95077091c07202856d4a9b920e7dc
mdsauce <max.dobeck@saucelabs.com>
2019-06-18 17:00:30 -0700

    updated formulae

--- schelper.rb
+++ schelper.rb
@@ -2,7 +2,7 @@ class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
   url "https://github.com/mdsauce/schelper/archive/v1.0.3.tar.gz"
-  sha256 "36d01c99ad9dea9abc5c96e3e43e91e21e2d36469ecc65d6f396b2e4d350cb49"
+  sha256 "0b59c8211421755f86098a0a621de69ec6385bf0ea8bddbe175bc0dc182594e0"
   depends_on "go" => :build
   version "1.0.3"
 
