63a5ba7a78f124699d220ed810f62f8338ae2c59
mdsauce <max.dobeck@saucelabs.com>
2019-06-18 17:14:06 -0700

    remove sha256 stuff.

--- schelper.rb
+++ schelper.rb
@@ -2,7 +2,6 @@ class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
   url "https://github.com/mdsauce/schelper/archive/v1.0.3.tar.gz"
-  sha256 "0b59c8211421755f86098a0a621de69ec6385bf0ea8bddbe175bc0dc182594e0"
   depends_on "go" => :build
   version "1.0.3"
 
